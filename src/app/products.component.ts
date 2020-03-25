e6d0805f4a10eb4dff5707c8b77f421b1e9bef11
Kateryna Bugaieva <SONNENSMILE@gmail.com>
2020-03-25 10:40:15 +0100

    forms

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ProductsService } from "./products.service";
 
 @Component({
   selector: "app-products",
@@ -9,14 +10,18 @@ export class ProductsComponent {
   isDisabled = true;
   products = ["A book", "A tree"];
 
-  constructor() {
+  constructor(private productsService: ProductsService) {
     setTimeout(() => {
       this.isDisabled = false;
     }, 3000);
   }
 
-  onAddProduct() {
-    this.products.push(this.productName);
+  onAddProduct(form) {
+    if (form.valid) {
+      console.log(form.value);
+      this.products.push(form.value.productName)
+      // this.productsService.addProduct()
+    }
   }
 
   onRemoveProduct(productName: string) {
