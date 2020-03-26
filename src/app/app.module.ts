59eab324fd7ff34e144591d5d9d4f989f8b8990d
Kateryna Bugaieva <SONNENSMILE@gmail.com>
2020-03-26 20:43:26 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { ProductComponent } from "./product/product.component";
-import { ProductsComponent } from "./products.component";
-import { ProductsService } from "./products.service";
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products.component';
+import { ProductsService } from './products.service';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, ProductsComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [ProductsService ],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ProductsComponent,
+    HomeComponent
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
